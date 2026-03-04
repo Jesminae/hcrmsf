5ffdd418684fd2d81e814cceb35eafdbe2764e1c
Jesmi <jesmi@gmail.com>
2026-03-04 23:14:33 +0530

    connected to railway mysql

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Jesmi@7012", // VERY IMPORTANT
-  database: "hcrms",
-  port: 3306                           // ADD THIS LINE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
@@ -13,7 +13,7 @@ db.connect((err) => {
     console.log("❌ Database connection failed");
     console.log("ERROR DETAILS:", err);
   } else {
-    console.log("✅ MySQL Connected");
+    console.log("✅ MySQL Connected to Railway");
   }
 });
 
