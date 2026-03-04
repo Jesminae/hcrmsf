892897d8925cfb0d63831c4af6f1d86c9a26934d
Jesmi <jesmi@gmail.com>
2026-03-04 23:39:59 +0530

    bind to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use("/api", attendanceRoutes);
 /* -------- SERVER -------- */
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
 
